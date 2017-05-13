8fa1cec50d8d8262498a1f53948f0d98883721fc
mnxanl <mnxanl@github.com>
2017-05-13 18:33:15 +0200

    Borrado de encuestas OK (probar bajo vuestra responsabilidad!)

--- Surveys/src/com/presentacio/ControladorPresentacio.java
+++ Surveys/src/com/presentacio/ControladorPresentacio.java
@@ -3,6 +3,7 @@ package com.presentacio;
 import com.domini.ControladorDominio;
 import com.domini.ExcFormatoIncorrecto;
 
+import javax.swing.*;
 import java.time.Instant;
 import java.util.Date;
 
@@ -14,10 +15,12 @@ public class ControladorPresentacio {
 
     LogIn li = null;
     VistaPrincipal vp = null;
+    private String criterio;
 
     public ControladorPresentacio() {
         try {
             ctrlDom = new ControladorDominio();
+            criterio = "A-Z";
         }
         catch (ExcFormatoIncorrecto e) {
             //mostrar error
@@ -36,14 +39,13 @@ public class ControladorPresentacio {
         else {
             li.close();
             vp = new VistaPrincipal(this, ctrlDom.getUser());
-
             vp.llenarLista(ctrlDom.listaEncuestas("A-Z"));
-
             vp.show();
         }
     }
 
     public void buscarEncuestas (String criterio) {
+        this.criterio = criterio;
         vp.llenarLista(ctrlDom.listaEncuestas(criterio));
     }
 
@@ -54,4 +56,13 @@ public class ControladorPresentacio {
     public void buscarEncuestaFecha (String f1, String f2) {
         vp.llenarLista(ctrlDom.listaEncuestaFecha(f1,f2));
     }
+
+    public void borrarEncuesta(String titulo) {
+        //JOptionPane.showMessageDialog(vp, "Eggs are not supposed to be green.");
+
+        ctrlDom.borrarEncuesta(titulo);
+        vp.llenarLista(ctrlDom.listaEncuestas(criterio));
+    }
+
+
 }
---
+++ Surveys/src/com/presentacio/VistaCrearEncuesta.java
@@ -0,0 +1,15 @@
+package com.presentacio;
+
+import javax.swing.*;
+
+public class VistaCrearEncuesta {
+    private JPanel panel1;
+    private JLabel userLabel;
+    private JTextField palClave;
+    private JList list1;
+    private JButton nuevaPreguntaButton;
+    private JButton borrarPreguntaButton;
+    private JButton modificarPreguntaButton;
+    private JPanel panelBotones;
+    private JTextArea textArea1;
+}
